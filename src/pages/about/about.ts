6ca06072b77e5ccd4ae6ca917cdafa9dd3c81cf9
jpinto7 <juan.pinto108@gmail.com>
2018-07-05 17:20:33 -0600

    Added graph to app

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'page-about',
@@ -11,4 +12,5 @@ export class AboutPage {
 
   }
 
+  onSubmit(f: NgForm) {}
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
+  //lineChartData = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  ionViewDidLoad() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Temperatura",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [],
+            spanGaps: false,
+          }
+        ]
+      },
+      options: {
+        scaleShowValues: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'second'
+            },
+            ticks: {
+              autoSkip: false
+            }
+          }]
+        }
+      }
+    });
+    setInterval(() => {
+      this.updateChart();
+    }, 5000);
+  }
+
+  updateChart() {
+    this.lineChart.data.labels.push(new Date());
+    this.lineChart.data.datasets.forEach(dataset => {
+      dataset.data.push(Math.random() * 10);
+    });
+    this.lineChart.update();
   }
 
 }
